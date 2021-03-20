33c08f4fe29de507faeaf6562dfdc5c963fd1277
Angrey Shoful <shoful.a@gmail.com>
2021-03-20 13:42:33 +0200

    fix format

--- src/Components/Tracker/format.js
+++ src/Components/Tracker/format.js
@@ -1,5 +1,8 @@
-export const format = (t) =>
-  `${pad(Math.floor((t / 1000 / 60 / 60) % 60))}:${pad(
-    Math.floor((t / 1000 / 60) % 60)
-  )}:${pad(Math.floor((t / 1000) % 60))}`;
+export const format = (t) => {
+  const p = t > 0 ? t : 0;
+  return `${pad(Math.floor((p / 1000 / 60 / 60) % 60))}:${pad(
+    Math.floor((p / 1000 / 60) % 60)
+  )}:${pad(Math.floor((p / 1000) % 60))}`;
+};
+
 const pad = (n) => (n < 10 ? `0${n}` : n);
